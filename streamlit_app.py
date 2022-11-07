519c1e7a93e7fd0c17c02eceb45f57aea3769850
DePiekartz <117635229+DePiekartz@users.noreply.github.com>
2022-11-07 10:26:21 +0100

    Update streamlit_app.py
    reset

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 
 import streamlit
 
-streamlit.title('My Parent New Healthy Diner')
+streamlit.title('My moms healthy diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & blueberry oatmeal')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado toast')
